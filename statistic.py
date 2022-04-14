a1440d3901ad024383dc6539cf4176189b8795f7
Keelo79 <xiao201105@163.com>
2022-04-14 20:45:15 +0800

    update statistic.py

--- statistic.py
+++ statistic.py
@@ -2,7 +2,10 @@ import torch
 import os
 from math import log10
 
-file_list = os.listdir('models')
+folder_name = 'models'
+# folder_name = 'best_model_storage'
+
+file_list = os.listdir(folder_name)
 for filename in file_list:
-    weight = torch.load('models/' + filename)
+    weight = torch.load(folder_name + '/' + filename)
     print(filename, weight['best'], 10 * log10(1 / weight['best'] ** 2))
